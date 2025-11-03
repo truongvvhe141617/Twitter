b19224ca7026ab545228900fb0a33e45d91c8ec1
truong.vv <truongsocui2k@gmail.com>
2025-11-03 22:03:14 +0700

    N/A update(base): Hello

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const name: string = 'Truong29'
-console.log(name)
+import express from 'express';
+
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send(`Hello world!`)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
